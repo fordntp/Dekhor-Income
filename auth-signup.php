e96a54f1c4a170a45a7110fb8b0acd58176155b1
taritinth <mars_1234_@hotmail.com>
2019-11-25 01:20:39 +0700

    commit day#1

--- auth-signup.php
+++ auth-signup.php
@@ -40,9 +40,12 @@
             <div class="card">
                 <div class="card-body text-center">
                     <div class="mb-4">
-                        <i class="feather icon-unlock auth-icon"></i>
+                        <i class="feather icon-user-plus auth-icon"></i>
                     </div>
-                    <h3 class="mb-4">Login</h3>
+                    <h3 class="mb-4">Sign up</h3>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Username">
+                    </div>
                     <div class="input-group mb-3">
                         <input type="email" class="form-control" placeholder="Email">
                     </div>
@@ -51,13 +54,18 @@
                     </div>
                     <div class="form-group text-left">
                         <div class="checkbox checkbox-fill d-inline">
-                            <input type="checkbox" name="checkbox-fill-1" id="checkbox-fill-a1" checked="">
-                            <label for="checkbox-fill-a1" class="cr"> Save Details</label>
+                            <input type="checkbox" name="checkbox-fill-1" id="checkbox-fill-1" checked="">
+                            <label for="checkbox-fill-1" class="cr"> Save Details</label>
                         </div>
                     </div>
-                    <button class="btn btn-primary shadow-2 mb-4">Login</button>
-                    <p class="mb-2 text-muted">Forgot password? <a href="auth-reset-password.html">Reset</a></p>
-                    <p class="mb-0 text-muted">Don’t have an account? <a href="auth-signup.html">Signup</a></p>
+                    <div class="form-group text-left">
+                        <div class="checkbox checkbox-fill d-inline">
+                            <input type="checkbox" name="checkbox-fill-2" id="checkbox-fill-2">
+                            <label for="checkbox-fill-2" class="cr">Send me the <a href="#!"> Newsletter</a> weekly.</label>
+                        </div>
+                    </div>
+                    <button class="btn btn-primary shadow-2 mb-4">Sign up</button>
+                    <p class="mb-0 text-muted">Allready have an account? <a href="auth-signin.html"> Log in</a></p>
                 </div>
             </div>
         </div>
