c0f037ac8e5a185907fa229ab090386147e71ac5
taritinth <mars_1234_@hotmail.com>
2019-11-26 00:11:16 +0700

    add js plugin

--- footer.php
+++ footer.php
@@ -3,5 +3,17 @@
 	<script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
     <script src="assets/js/pcoded.min.js"></script>
 
+    <script src="assets/plugins/modal-window-effects/js/classie.js"></script>
+    <script src="assets/plugins/modal-window-effects/js/modalEffects.js"></script>
+
+    <script src="assets/plugins/smart-wizard/js/jquery.smartWizard.min.js"></script>
+    <script src="assets/js/pages/wizard-custom.js"></script>
+
+    <script src="assets/plugins/inputmask/js/inputmask.min.js"></script>
+    <script src="assets/plugins/inputmask/js/jquery.inputmask.min.js"></script>
+    <script src="assets/plugins/inputmask/js/autoNumeric.js"></script>
+
+    <script src="assets/js/pages/form-masking-custom.js"></script>
+
 </body>
 </html>
