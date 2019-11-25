ef7becc485399859b52e75e9b86463c2e50a6187
taritinth <mars_1234_@hotmail.com>
2019-11-26 01:24:56 +0700

    add float value to db
    normalizeDecimal

--- include/database.php
+++ include/database.php
@@ -1,11 +1,24 @@
 <?php
 include 'config.php';
 
-$conn = mysqli_connect("$host","$dbuser","$dbpass","$dbtable");
-mysqli_query($conn,"SET NAMES UTF8");
+$conn = mysqli_connect("$host", "$dbuser", "$dbpass", "$dbtable");
+mysqli_query($conn, "SET NAMES UTF8");
 
-
-if(mysqli_connect_error())
+if (mysqli_connect_error()) {
     echo "***SYSTEM : CONNECTION_ERROR***";
+}
 
- $get_month = date('m');
+$get_date = date("Y-m-d H:i:s");
+$get_month = date('m');
+
+function normalizeDecimal($val, int $precision = 2): string
+{
+    $input = str_replace(' ', '', $val);
+    $number = str_replace(',', '.', $input);
+    if (strpos($number, '.')) {
+        $groups = explode('.', str_replace(',', '.', $number));
+        $lastGroup = array_pop($groups);
+        $number = implode('', $groups) . '.' . $lastGroup;
+    }
+    return bcadd($number, 0, $precision);
+}
