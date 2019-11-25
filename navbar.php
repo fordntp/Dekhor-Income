a3e77d0190e8a551cb35adc053fe2e8c7b2b8723
fordntp <56291836+fordntp@users.noreply.github.com>
2019-11-25 23:34:45 +0700

    navigation bar

---
+++ navbar.php
@@ -0,0 +1,54 @@
+    <!-- [ navbar header ] start -->
+    <header class="navbar pcoded-header navbar-expand-lg navbar-light headerpos-fixed header-blue">
+        <div class="m-header">
+            <a class="mobile-menu" id="mobile-collapse1" href="javascript:"><span></span></a>
+            <a href="https://tarit.in.th/dekhor/" class="b-brand">
+                   <div class="b-bg">
+                       <i class="feather icon-trending-up"></i>
+                   </div>
+                   <span class="b-title">Dek Hor</span>
+               </a>
+        </div>
+        <a class="mobile-menu" id="mobile-header" href="javascript:">
+            <i class="feather icon-more-horizontal"></i>
+        </a>
+        <!-- <div class="collapse navbar-collapse">
+            <ul class="navbar-nav mr-auto">
+                <li><a href="javascript:" class="full-screen" onclick="javascript:toggleFullScreen()"><i class="feather icon-maximize"></i></a></li>
+                <li>
+                    <div class="dropdown drp-user">
+                        <a class="dropdown-toggle" href="javascript:" data-toggle="dropdown" aria-expanded="false">พฤศจิกายน</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="javascript:">Action</a></li>
+                            <li><a class="dropdown-item" href="javascript:">Another action</a></li>
+                            <li><a class="dropdown-item" href="javascript:">Something else here</a></li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li>
+                    <div class="dropdown drp-user">
+                        <a href="javascript:" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                            <i class="icon feather icon-settings"></i>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right profile-notification">
+                            <div class="pro-head">
+                                <img src="assets/images/user/avatar-1.jpg" class="img-radius" alt="User-Profile-Image">
+                                <span>ธฤต ขานโบ</span>
+                                <a href="javascript:" class="dud-logout" title="ออกจากระบบ">
+                                    <i class="feather icon-log-out"></i>
+                                </a>
+                            </div>
+                            <ul class="pro-body">
+                                <li><a href="javascript:" class="dropdown-item"><i class="feather icon-settings"></i> ตั้งค่า</a></li>
+                                <li><a href="javascript:" class="dropdown-item"><i class="feather icon-user"></i> โปรไฟล์</a></li>
+                                <li><a href="javascript:" class="dropdown-item"><i class="feather icon-log-out"></i> ออกจากระบบ</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div> -->
+    </header>
+    <!-- [ navbar header ] end -->
