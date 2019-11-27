e72dc2e658ae1000aa7c305d41d453a677898838
taritinth <mars_1234_@hotmail.com>
2019-11-27 23:30:22 +0700

    add src js/app.js

--- footer.php
+++ footer.php
@@ -1,21 +1,28 @@
+    <!-- App Js -->
+    <script src="assets/js/app.js"></script>
+
     <!-- Required Js -->
     <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
     <script src="assets/js/vendor-all.min.js"></script>
 	<script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
     <script src="assets/js/pcoded.min.js"></script>
 
+    <!--Modal Effect-->
     <script src="assets/plugins/modal-window-effects/js/classie.js"></script>
     <script src="assets/plugins/modal-window-effects/js/modalEffects.js"></script>
 
+    <!--Smart Wizard-->
     <script src="assets/plugins/smart-wizard/js/jquery.smartWizard.min.js"></script>
     <script src="assets/js/pages/wizard-custom.js"></script>
 
+    <!--InputMask-->
     <script src="assets/plugins/inputmask/js/inputmask.min.js"></script>
     <script src="assets/plugins/inputmask/js/jquery.inputmask.min.js"></script>
     <script src="assets/plugins/inputmask/js/autoNumeric.js"></script>
 
     <script src="assets/js/pages/form-masking-custom.js"></script>
 
+    <!--SweerAlert-->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9/dist/sweetalert2.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@9/dist/sweetalert2.min.css" id="theme-styles">
 
