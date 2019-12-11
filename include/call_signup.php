63135dde032a0188ba9583ad21a176087f71da90
yachtdome <56293666+yachtdome@users.noreply.github.com>
2019-12-11 23:22:25 +0700

    add call_signup

---
+++ include/call_signup.php
@@ -0,0 +1,36 @@
+<?php
+    include 'database.php';
+
+    $username = $_REQUEST['username'];
+    $password_1 = $_REQUEST['password_1'];
+    $password_2 = $_REQUEST['password_2'];
+
+    $cmd_check = "SELECT username FROM dekhor_user WHERE username = '$username';";
+    $qry_check = mysqli_query($conn,$cmd_check);
+    $rows = mysqli_num_rows($qry_check);
+    if($rows > 0)
+        echo 0 // already have user
+    else
+    {
+
+            $cmd_insert = "INSERT INTO dekhor_user (username,password,role) VALUES ('$username','$password_1',1);";
+            $qry_insert = mysqli_query($conn,$cmd_insert);
+
+            // AUTO CREATE WALLET
+            $cmd_select = "SELECT * FROM dekhor_user WHERE username = '$username' ;";
+            $qry_select = mysqli_query($conn,$cmd_select);
+            while($data = mysqli_fetch_array($qry_select))
+            {
+                $user_id = $data['id'];
+            }
+
+            $cmd_insert2 = "INSERT INTO dekhor_wallet (type,user_id,wallet_name,balance,create_date) ('wallet','$user_id','PiggyBank','0.00',now());";
+            $qry_select2 = mysqli_query($conn,$cmd_insert2);
+             
+            echo 1 // complete
+
+
+            
+    }
+
+?>
