e7956456a24a2344a51897cef124b41c3aaa3e42
taritinth <mars_1234_@hotmail.com>
2019-12-13 01:10:41 +0700

    define user_id

--- include/call_auth.php
+++ include/call_auth.php
@@ -1,28 +1,20 @@
 <?php
 
-    include 'database.php';
-    session_start();
-    $user = $_REQUEST['username'];
-    $pass = $_REQUEST['password'];
-    $user = mysqli_real_escape_string($conn,$user);
-    $pass = mysqli_real_escape_string($conn,$pass);
-    $info = array();
-    $cmd = "SELECT *,b.id as wallet_id FROM dekhor_user a JOIN dekhor_wallet b ON a.id = b.user_id WHERE username='$user' && password = '$pass' ORDER BY b.id ASC;";
-    $qry = mysqli_query($conn,$cmd);
-    $RowCheck = mysqli_num_rows($qry);
-    if($RowCheck > 0)
-    {
-        $data = mysqli_fetch_array($qry);
-        $_SESSION['user_id'] = $data['id'];
-        $_SESSION['wallet_id'] = $data['wallet_id'];
-        echo 1;
-    }
-    else
-    {
-        echo 0;
-    }
-
-
-    
-    
-?>
+include 'database.php';
+session_start();
+$user = $_REQUEST['username'];
+$pass = $_REQUEST['password'];
+$user = mysqli_real_escape_string($conn, $user);
+$pass = mysqli_real_escape_string($conn, $pass);
+$info = array();
+$cmd = "SELECT *, b.id as wallet_id, a.id as user_id FROM dekhor_user a JOIN dekhor_wallet b ON a.id = b.user_id WHERE username='$user' && password = '$pass' ORDER BY b.id ASC;";
+$qry = mysqli_query($conn, $cmd);
+$RowCheck = mysqli_num_rows($qry);
+if ($RowCheck > 0) {
+    $data = mysqli_fetch_array($qry);
+    $_SESSION['user_id'] = $data['user_id'];
+    $_SESSION['wallet_id'] = $data['wallet_id'];
+    echo 1;
+} else {
+    echo 0;
+}
