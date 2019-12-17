b778b8a2bd9b1caa000c76eb0cad311aeadc874d
fordntp <62070065@kmitl.ac.th>
2019-12-17 16:12:53 +0700

    update lastlogin && ip address
    Co-Authored-By: fordntp <fordntp@users.noreply.github.com>

--- include/database.php
+++ include/database.php
@@ -24,3 +24,33 @@ function normalizeDecimal($val, int $precision = 2): string
     }
     return bcadd($number, 0, $precision);
 }
+
+function getRealip()
+{
+    if (isset($_SERVER)) {
+        if (isset($_SERVER["HTTP_X_FORWARDED_FOR"])) {
+            $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
+            if (strpos($ip, ",")) {
+                $exp_ip = explode(",", $ip);
+                $ip = $exp_ip[0];
+            }
+        } else if (isset($_SERVER["HTTP_CLIENT_IP"])) {
+            $ip = $_SERVER["HTTP_CLIENT_IP"];
+        } else {
+            $ip = $_SERVER["REMOTE_ADDR"];
+        }
+    } else {
+        if (getenv('HTTP_X_FORWARDED_FOR')) {
+            $ip = getenv('HTTP_X_FORWARDED_FOR');
+            if (strpos($ip, ",")) {
+                $exp_ip = explode(",", $ip);
+                $ip = $exp_ip[0];
+            }
+        } else if (getenv('HTTP_CLIENT_IP')) {
+            $ip = getenv('HTTP_CLIENT_IP');
+        } else {
+            $ip = getenv('REMOTE_ADDR');
+        }
+    }
+    return $ip;
+}
