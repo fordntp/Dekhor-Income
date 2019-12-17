10dc6a028428cc9471dfa91d39a9d5e7df44dc50
fordntp <62070065@kmitl.ac.th>
2019-12-17 16:12:44 +0700

    update lastlogin && ip address
    Co-Authored-By: fordntp <fordntp@users.noreply.github.com>

--- include/call_auth.php
+++ include/call_auth.php
@@ -2,6 +2,7 @@
 
 include 'database.php';
 session_start();
+$ip = getRealip();
 $user = $_REQUEST['username'];
 $pass = $_REQUEST['password'];
 $user = mysqli_real_escape_string($conn, $user);
@@ -15,7 +16,12 @@ if ($RowCheck > 0) {
     $_SESSION['user_id'] = $data['user_id'];
     $_SESSION['wallet_id'] = $data['wallet_id'];
     $_SESSION['uname'] = $data['username'];
+
+    //update lastlogin && ip address
+    $cmd1 = "UPDATE dekhor_user SET last_login = '$get_date' , ip = '$ip' WHERE id = '" . $_SESSION['user_id'] . "'";
+    $qry1 = mysqli_query($conn, $cmd1);
     echo 1;
+
 } else {
     echo 0;
 }
